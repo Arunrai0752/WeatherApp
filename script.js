6c62b243079f39100eeac569f737efa3b9566d50
Arunrai0752 <realarunrai0752@gmail.com>
2025-05-23 18:28:51 +0530

    UPLOAD

---
+++ script.js
@@ -0,0 +1,43 @@
+const API_KEY = "4835f023f6ad54b2303416425106da62";
+
+async function getWeather() {
+    const city = document.getElementById("city").value.trim();
+
+    try {
+        const { lat, lon } = await getGeoLoc(city);
+
+        if (!lat || !lon) {
+            alert("Invalid City Name: " + city);
+            document.getElementById("city").value = "";
+            return;
+        }
+
+        const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        document.getElementById("temp").innerText = (data.main.temp - 273.14).toFixed(2);
+        document.getElementById("windSpeed").innerText = data.wind.speed + " m/s";
+        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    } catch (error) {
+        console.log("An Error Occurred", error);
+    }
+}
+
+async function getGeoLoc(city) {
+    try {
+        const URL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+
+        console.log(lat,lon);
+        
+        return { lat, lon };
+    } catch (error) {
+        console.log(error);
+        return {};
+    }
+}
