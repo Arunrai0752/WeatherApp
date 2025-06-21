f4d6b630728e1652a483d5d7c73dc9e268ef4f74
Arunrai0752 <realarunrai0752@gmail.com>
2025-06-21 14:09:37 +0530

    Nothig change

--- script.js
+++ script.js
@@ -41,3 +41,20 @@ async function getGeoLoc(city) {
         return {};
     }
 }
+
+
+// function outer() {
+//   let count = 0; 
+//   return function inner() {
+//     count++;     
+//     return count;
+//   };
+// }
+
+// const counter = outer(); 
+
+// console.log(counter()); 
+// console.log(counter()); 
+// console.log(counter()); 
+// console.log(counter()); 
+// console.log(counter()); 
